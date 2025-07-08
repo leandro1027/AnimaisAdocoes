abf22c5d8742bd730a218c3a3f4d49ab3ba0ab58
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-07-08 20:40:37 -0300

    usuarios dtos

--- Back-end/src/usuario/dto/create-usuario.dto.ts
+++ Back-end/src/usuario/dto/create-usuario.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUsuarioDto {}
+import { IsNotEmpty, IsNumber, IsPhoneNumber, IsString, MaxLength } from "class-validator"
+
+export class CreateUsuarioDto {
+
+    @IsString()
+    @MaxLength(20, {message: "O nome precisa ter no máximo 20 caracteres!"})
+    @IsNotEmpty({message: "O nome não pode ser vazio!"})
+    readonly nome: string
+
+    @IsNumber()
+    @IsNotEmpty({message: "A idade não pode ser vazia!"})
+    readonly idade: number
+
+    @IsString()
+    @MaxLength(40, {message: "O endereço precisa ter no máximo 40 caracteres!"})
+    @IsNotEmpty({message: "O endereço não pode ser vazio!"})
+    readonly endereco: string
+
+    @IsPhoneNumber()
+    @IsNotEmpty({message: "O telefone não pode ser vazio!"})
+    readonly telefone: string 
+}
--- Back-end/src/usuario/dto/update-usuario.dto.ts
+++ Back-end/src/usuario/dto/update-usuario.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUsuarioDto } from './create-usuario.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {}
+export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {
+
+    @IsOptional()
+    @IsBoolean()
+    readonly adotou? : boolean
+}
