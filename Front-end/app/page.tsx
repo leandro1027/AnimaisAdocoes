68f173132769383b1a21d47746769100682aa75a
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-07-15 12:16:36 -0300

    Update page.tsx

--- Front-end/app/page.tsx
+++ Front-end/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-blue-50 flex items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-600">
-        Tailwind está funcionando! 🎉
+        Olá, acesse as rotas!
       </h1>
     </div>
   );
